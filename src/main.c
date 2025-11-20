598f92ad62d6294b7ee79cba02ef78707bad4507
Felipe Alves <felipefas209@outlook.com>
2025-11-20 12:58:52 -0300

    ADD: Menu principal do programa adicionado

--- src/main.c
+++ src/main.c
@@ -4,16 +4,51 @@
 
 int main()
 {
+    // Variáveis iniciais
+    int opt = 0;
     Catalogo *catalogo = (Catalogo*) malloc(sizeof(Catalogo)); // Catálogo onde ficará as especificações dos produtos da rede
-    i_catalog(catalogo); // inicia o catálogo
+    DBLojas *db = db_aloc();
+    Carrinho *car = c_aloc();
 
-    // Testando se o catálogo está em funcionamento
-    Item *aux = catalogo->p_item;
-    while(aux != NULL)
+    // Fluxo do programa
+    while(opt >= 0)
     {
-        printf("%d %s %f\n", aux->id, aux->nome, aux->valor);
+        printf("\nBem-vindo ao sistema de estoque! A seguir as opções:\n");
+        printf("\n[1] Carregar dados\n[2] Pesquisa\n[3] Carrinho\n[4] Verificar disponibilidade\n[5]Finalizar compra\n[6] Relatórios\n");
+        printf("\nDigite aqui: ");
+        scanf("%d", &opt);
 
-        aux = aux->prox;
+        switch(opt)
+        {
+            case 1:
+                {
+                    carrega(catalogo, db);
+                } break;
+            case 2:
+                {
+                    pesquisa();
+                } break;
+            case 3:
+                {
+                    carrinho();
+                } break;
+            case 4:
+                {
+                    verifica_dis();
+                } break;
+            case 5:
+                {
+                    finaliza();
+                } break;
+            case 6:
+                {
+                    relatorio();
+                } break;
+            default:
+                {
+                    printf("\nComando não encontrado, tente novamente.\n");
+                } break;
+        }
     }
 
     return 0;
